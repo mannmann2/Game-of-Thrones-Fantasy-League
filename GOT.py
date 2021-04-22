add65ecd8a2cc46ac91e666ce6eb1dc824a4b7cf
Arman <armanmann2@gmail.com>
2021-04-22 19:46:23 +0530

    who cares...

---
+++ GOT.py
@@ -0,0 +1,174 @@
+#!/Users/tom/anaconda3/bin/python
+
+week1= '''
+abidanzar@gmail.com	Arya Stark	Jon Snow	Bran Stark	Daenerys Targaryen	Sansa Stark	Cersei Lannister	Drogon	The Night King	Tyrion Lannister	Jaime Lannister	The Hound	Grey Worm	Rheagal	Tormund Giantsbane	The Mountain	Brienne of Tarth	Bronn	Beric Dondarrion	Jorah Mormont	Davos Seaworth	Euron Greyjoy	Gendry	Missandei	Varys	Podrick Payne
+cage.reaper05@gmail.com	Jon Snow	Arya Stark	Cersei Lannister	Bran Stark	Daenerys Targaryen	Tyrion Lannister	Davos Seaworth	Jaime Lannister	Brienne of Tarth	Jorah Mormont	Qyburn	The Hound	Tormund Giantsbane	Samwell Tarly	Sansa Stark	Nymeria (wolf)	Grey Worm	Euron Greyjoy	Ghost	Gendry	Missandei	The Night King	Bronn	Varys	Theon Greyjoy
+abhirup.7@gmail.com	Jon Snow	Arya Stark	Cersei Lannister	Daenerys Targaryen	Drogon	Jaime Lannister	The Night King	Bran Stark	Tyrion Lannister	Viserion	Sansa Stark	Euron Greyjoy	Grey Worm	Bronn	Brienne of Tarth	Davos Seaworth	Melisandre	Varys	Tormund Giantsbane	The Hound	The Mountain	Brienne of Tarth	Beric Dondarrion	Samwell Tarly	Jorah Mormont
+nishant0729@gmail.com	Tyrion Lannister	Jon Snow	Daenerys Targaryen	Cersei Lannister	Arya Stark	Bran Stark	Sansa Stark	The Night King	Theon Greyjoy	Brienne of Tarth	Jaime Lannister	Grey Worm	Missandei	Jorah Mormont	Samwell Tarly	The Mountain	Tormund Giantsbane	The Hound	Bronn	Euron Greyjoy	Varys	Lyanna Mormont	Melisandre	Viserion	Drogon
+rebeccasamverghese@gmail.com	The Night King	Viserion	Jon Snow	Arya Stark	Daenerys Targaryen	Sansa Stark	Samwell Tarly	Bran Stark	Cersei Lannister	Tyrion Lannister	Brienne of Tarth	Beric Dondarrion	Bronn	Drogon	Gendry	Jaime Lannister	Jorah Mormont	Grey Worm	Missandei	The Hound	The Mountain	Willa	Varys	Qyburn	Rheagal
+gaganberi@gmail.com	Jon Snow	Daenerys Targaryen	Bran Stark	The Night King	Viserion	Samwell Tarly	Tyrion Lannister	Cersei Lannister	The Mountain	Jaime Lannister	The Hound	Theon Greyjoy	Rheagal	Ebrose	Jorah Mormont	Yara Greyjoy	Gendry	Varys	Brienne of Tarth	Gilly	Nymeria (wolf)	Melisandre	Qyburn	Podrick Payne	Ned Umber
+pranshupoddar@gmail.com	Jon Snow	The Night King	Daenerys Targaryen	Tyrion Lannister	Arya Stark	Cersei Lannister	Sansa Stark	Bran Stark	Samwell Tarly	Eddison Tollett	The Hound	Beric Dondarrion	Tormund Giantsbane	Jaime Lannister	Euron Greyjoy	Bronn	Viserion	Rheagal	Jorah Mormont	The Mountain	Lyanna Mormont	Brienne of Tarth	Drogon	Grey Worm	Davos Seaworth
+ishaan27c@gmail.com	The Night King	Bran Stark	Jaime Lannister	Euron Greyjoy	Sansa Stark	Viserion	Cersei Lannister	Harry Strickland	Melisandre	Samwell Tarly	Theon Greyjoy	Qyburn	Drogon	The Hound	Beric Dondarrion	Varys	Tormund Giantsbane	Grey Worm	Arya Stark	Yara Greyjoy	Tyrion Lannister	Brienne of Tarth	Jon Snow	Hot Pie	Daenerys Targaryen
+armanmann2@gmail.com	Bran Stark	Jaime Lannister	The Night King	Samwell Tarly	Theon Greyjoy	Euron Greyjoy	Tyrion Lannister	Jon Snow	Daenerys Targaryen	Jorah Mormont	Arya Stark	Sansa Stark	The Hound	Beric Dondarrion	Tormund Giantsbane	Bronn	Viserion	Drogon	Qyburn	Yara Greyjoy	Melisandre	Grey Worm	Gendry	Ghost	Varys
+dark.avipdo@gmail.com	Drogon	The Night King	Jon Snow	Viserion	Grey Worm	The Mountain	Arya Stark	Jaime Lannister	Daenerys Targaryen	Bran Stark	Cersei Lannister	Bronn	Rheagal	Jorah Mormont	Harry Strickland	Sansa Stark	Tormund Giantsbane	The Hound	Qyburn	Brienne of Tarth	Ghost	Nymeria (wolf)	Euron Greyjoy	Gendry	Melisandre
+alex.shaji@gmail.com	Sansa Stark	Jaime Lannister	Cersei Lannister	Daenerys Targaryen	Jon Snow	Tyrion Lannister	Arya Stark	Drogon	The Night King	Viserion	Bran Stark	Qyburn	Euron Greyjoy	Davos Seaworth	The Hound	The Mountain	Varys	Grey Worm	Brienne of Tarth	Bronn	Gendry	Ghost	Samwell Tarly	Yara Greyjoy	Tormund Giantsbane
+aniformer@gmail.com	Bran Stark	Bronn	Viserion	Euron Greyjoy	The Night King	Arya Stark	Jaime Lannister	Tormund Giantsbane	Tyrion Lannister	Cersei Lannister	Varys	Daenerys Targaryen	Yara Greyjoy	Jon Snow	Sansa Stark	The Hound	Lyanna Mormont	Qyburn	Brienne of Tarth	Davos Seaworth	Jorah Mormont	Tycho Nestoris	Theon Greyjoy	Melisandre	Theon Greyjoy
+'''
+
+
+week2 = '''
+rebeccasamverghese@gmail.com	Jon Snow	Daenerys Targaryen	Sansa Stark	Jaime Lannister	Bran Stark	Beric Dondarrion	Tormund Giantsbane	Tyrion Lannister	Arya Stark	The Night King	Viserion	Samwell Tarly	Drogon	Jorah Mormont	Rheagal	Euron Greyjoy	Cersei Lannister	Eddison Tollett	The Hound	Bronn	The Mountain	Brienne of Tarth	Davos Seaworth	Gendry	Grey Worm
+dark.avipdo@gmail.com	Jon Snow	Drogon	Bran Stark	Cersei Lannister	Euron Greyjoy	Harry Strickland	Sansa Stark	Davos Seaworth	Daenerys Targaryen	Viserion	The Night King	The Hound	The Mountain	Tormund Giantsbane	Tyrion Lannister	Samwell Tarly	Rheagal	Jorah Mormont	Bronn	Theon Greyjoy	Brienne of Tarth	Beric Dondarrion	Arya Stark	Melisandre	Rheagal
+pranshupoddar@gmail.com	Jon Snow	Bran Stark	Jaime Lannister	Daenerys Targaryen	The Night King	Sansa Stark	Tyrion Lannister	Cersei Lannister	Samwell Tarly	Jorah Mormont	Theon Greyjoy	Euron Greyjoy	Viserion	Arya Stark	Drogon	Beric Dondarrion	Tormund Giantsbane	Eddison Tollett	Melisandre	Varys	The Hound	Harry Strickland	Brienne of Tarth	Qyburn	Ghost
+abidanzar@gmail.com	Jon Snow	Jaime Lannister	Bran Stark	The Night King	Arya Stark	Sansa Stark	Grey Worm	Tormund Giantsbane	Tyrion Lannister	The Hound	Drogon	Brienne of Tarth	Daenerys Targaryen	Gendry	Davos Seaworth	Varys	Jorah Mormont	Samwell Tarly	Qyburn	Theon Greyjoy	Beric Dondarrion	Bronn	Rheagal	Drogon	Missandei
+cage.reaper05@gmail.com	Arya Stark	Jon Snow	Bran Stark	Daenerys Targaryen	Davos Seaworth	Jaime Lannister	Lyanna Mormont	Gendry	Grey Worm	Harry Strickland	Rheagal	The Night King	Tyrion Lannister	Tormund Giantsbane	Samwell Tarly	Sansa Stark	The Hound	Cersei Lannister	The Mountain	Drogon	Jorah Mormont	Theon Greyjoy	Euron Greyjoy	Melisandre	Missandei
+gaganberi@gmail.com	Arya Stark	Cersei Lannister	Jon Snow	The Night King	Daenerys Targaryen	Bran Stark	Drogon	Gendry	Sansa Stark	Samwell Tarly	Eddison Tollett	Bronn	Drogon	Tyrion Lannister	Yara Greyjoy	Theon Greyjoy	Robin Arryn	Beric Dondarrion	Jaime Lannister	Qyburn	Grey Worm	Euron Greyjoy	Rheagal	Ebrose	Varys
+alex.shaji@gmail.com	Tormund Giantsbane	Jon Snow	Daenerys Targaryen	Jaime Lannister	Sansa Stark	Bran Stark	Arya Stark	Rheagal	Drogon	Bronn	Samwell Tarly	Beric Dondarrion	Gendry	Qyburn	Euron Greyjoy	Jorah Mormont	Grey Worm	The Mountain	The Hound	Melisandre	Meera Reed	Davos Seaworth	The Night King	Yara Greyjoy	Theon Greyjoy
+nishant0729@gmail.com	Daenerys Targaryen	Jon Snow	Jaime Lannister	Cersei Lannister	Sansa Stark	Arya Stark	Samwell Tarly	Euron Greyjoy	Bran Stark	Theon Greyjoy	Varys	Tyrion Lannister	Yara Greyjoy	Alys Karstark	Brienne of Tarth	Gendry	Lyanna Mormont	Jorah Mormont	Missandei	Grey Worm	Drogon	Beric Dondarrion	Tormund Giantsbane	The Hound	Varys
+abhirup.7@gmail.com	Jaime Lannister	Jon Snow	Bran Stark	Daenerys Targaryen	Sansa Stark	Cersei Lannister	Arya Stark	Euron Greyjoy	Beric Dondarrion	Tormund Giantsbane	The Night King	Grey Worm	Drogon	Rheagal	Tyrion Lannister	Samwell Tarly	Gendry	Qyburn	Davos Seaworth	Brienne of Tarth	Bronn	Jorah Mormont	Theon Greyjoy	Varys	Viserion
+armanmann2@gmail.com	Daenerys Targaryen	Arya Stark	Sansa Stark	Jaime Lannister	Bran Stark	Tormund Giantsbane	Jorah Mormont	Grey Worm	Beric Dondarrion	Eddison Tollett	Bronn	The Hound	Theon Greyjoy	Tyrion Lannister	Davos Seaworth	Harry Strickland	Lyanna Mormont	Gendry	Samwell Tarly	Brienne of Tarth	Yara Greyjoy	Drogon	Viserion	Ghost	Qyburn
+aniformer@gmail.com	Tormund Giantsbane	Jon Snow	Jaime Lannister	Tyrion Lannister	Euron Greyjoy	The Night King	Bronn	Arya Stark	Cersei Lannister	Sansa Stark	Daenerys Targaryen	Drogon	Yara Greyjoy	Samwell Tarly	Beric Dondarrion	The Hound	Davos Seaworth	Brienne of Tarth	Harry Strickland	Varys	Viserion	Jorah Mormont	Missandei	Grey Worm	Ebrose
+ishaan27c@gmail.com	Jaime Lannister	Daenerys Targaryen	The Night King	Bran Stark	Sansa Stark	Tormund Giantsbane	Arya Stark	Grey Worm	Viserion	The Hound	Tyrion Lannister	Samwell Tarly	Missandei	Davos Seaworth	Gendry	Drogon	Robett Glover	Meera Reed	Yara Greyjoy	Brienne of Tarth	Alys Karstark	Gilly	Theon Greyjoy	Rheagal	Jon Snow
+'''
+
+week3 = '''
+abhirup.7@gmail.com	Drogon	Jon Snow	Arya Stark	Bran Stark	The Night King	Viserion	Daenerys Targaryen	Rheagal	Tormund Giantsbane	Brienne of Tarth	Jaime Lannister	Grey Worm	Gendry	Ghost	Beric Dondarrion	Theon Greyjoy	Podrick Payne	The Hound	Jorah Mormont	Samwell Tarly	Sansa Stark	Tyrion Lannister	Davos Seaworth	Euron Greyjoy	Bronn
+pranshupoddar@gmail.com	The Night King	Jon Snow	Arya Stark	Drogon	Viserion	Daenerys Targaryen	Jaime Lannister	Beric Dondarrion	Theon Greyjoy	Ghost	Jorah Mormont	Grey Worm	Tormund Giantsbane	Rheagal	Brienne of Tarth	The Hound	Gendry	Podrick Payne	Lyanna Mormont	Bran Stark	Davos Seaworth	Samwell Tarly	Eddison Tollett	Sansa Stark	Tyrion Lannister
+armanmann2@gmail.com	Jon Snow	Arya Stark	Brienne of Tarth	Jaime Lannister	Tormund Giantsbane	The Night King	Theon Greyjoy	Jorah Mormont	The Hound	Gendry	Eddison Tollett	Drogon	Grey Worm	Lyanna Mormont	Beric Dondarrion	Samwell Tarly	Davos Seaworth	Podrick Payne	Ghost	Sansa Stark	Tyrion Lannister	Bran Stark	Daenerys Targaryen	Qhono	Yohn Royce
+abidanzar@gmail.com	The Night King	Jon Snow	Arya Stark	Jaime Lannister	Daenerys Targaryen	Bran Stark	Drogon	Brienne of Tarth	Tyrion Lannister	Samwell Tarly	Rheagal	Grey Worm	Theon Greyjoy	Tormund Giantsbane	Viserion	Podrick Payne	Beric Dondarrion	Missandei	Davos Seaworth	Jorah Mormont	Sansa Stark	Gendry	Qhono	Varys	Ghost
+cage.reaper05@gmail.com	Jon Snow	Jaime Lannister	Bran Stark	The Night King	Arya Stark	Gendry	Daenerys Targaryen	Jorah Mormont	Brienne of Tarth	Drogon	Beric Dondarrion	Grey Worm	Theon Greyjoy	Sansa Stark	The Hound	Tyrion Lannister	Tormund Giantsbane	Samwell Tarly	Rheagal	Podrick Payne	Gilly	Missandei	Meera Reed	Nymeria (wolf)	Melisandre
+dark.avipdo@gmail.com	Jon Snow	Brienne of Tarth	Drogon	Rheagal	Jorah Mormont	Arya Stark	Jaime Lannister	Grey Worm	The Hound	The Night King	Tormund Giantsbane	Beric Dondarrion	Bran Stark	Davos Seaworth	Daenerys Targaryen	Viserion	Tyrion Lannister	Theon Greyjoy	Sansa Stark	Samwell Tarly	Bronn	Ghost	Nymeria (wolf)	Lyanna Mormont	Missandei
+nishant0729@gmail.com	Jon Snow	Arya Stark	The Night King	Brienne of Tarth	Bran Stark	Drogon	Tormund Giantsbane	The Hound	Viserion	Beric Dondarrion	Jorah Mormont	Theon Greyjoy	Gendry	Daenerys Targaryen	Tyrion Lannister	Jaime Lannister	Sansa Stark	Podrick Payne	Rheagal	Samwell Tarly	Grey Worm	Lyanna Mormont	Bronn	Melisandre	Varys
+aniformer@gmail.com	Jon Snow	Tormund Giantsbane	Daenerys Targaryen	Brienne of Tarth	Arya Stark	The Night King	Jaime Lannister	Viserion	Bran Stark	Drogon	Sansa Stark	Jorah Mormont	The Hound	Beric Dondarrion	Davos Seaworth	Ghost	Lyanna Mormont	Grey Worm	Podrick Payne	Nymeria (wolf)	Varys	Gendry	Yohn Royce	Tycho Nestoris
+gaganberi@gmail.com	The Night King	Daenerys Targaryen	Jaime Lannister	Jon Snow	Arya Stark	Drogon	Brienne of Tarth	Viserion	Theon Greyjoy	Samwell Tarly	Tyrion Lannister	Rheagal	Davos Seaworth	Jorah Mormont	The Mountain	Gendry	Grey Worm	Varys	The Hound	Podrick Payne	Lyanna Mormont	Beric Dondarrion	Sansa Stark	Qyburn	Alys Karstark
+rebeccasamverghese@gmail.com	Jon Snow	Daenerys Targaryen	Arya Stark	The Night King	Bran Stark	Jaime Lannister	Brienne of Tarth	Tyrion Lannister	Sansa Stark	Theon Greyjoy	Melisandre	Tormund Giantsbane	The Hound	Samwell Tarly	Jorah Mormont	Davos Seaworth	Grey Worm	Lyanna Mormont	Drogon	Qhono	Rheagal	Beric Dondarrion	Gendry	Ghost	Varys
+ishaan27c@gmail.com	The Night King	Arya Stark	Jaime Lannister	Brienne of Tarth	Grey Worm	Beric Dondarrion	Viserion	Theon Greyjoy	Tormund Giantsbane	Eddison Tollett	Davos Seaworth	Drogon	Gendry	Lyanna Mormont	Jorah Mormont	The Hound	Varys	Gilly	Melisandre	Missandei	Sansa Stark	Yara Greyjoy	Yohn Royce	Rheagal	Daenerys Targaryen
+alex.shaji@gmail.com	Jaime Lannister	Theon Greyjoy	The Hound	Alys Karstark	Eddison Tollett	Jorah Mormont	Gendry	Tormund Giantsbane	Ghost	Beric Dondarrion	Bronn	Drogon	Euron Greyjoy	Ghost	Gilly	Ebrose	Alys Karstark	Arya Stark	Drogon	Brienne of Tarth	Grey Worm	Cersei Lannister	Jon Snow	Daenerys Targaryen	Bran Stark
+'''
+
+
+import os
+import time
+import random
+import pandas as pd
+from collections import Counter
+
+
+WEEK = week3
+df = pd.DataFrame(WEEK.split('\n')[1:-1])
+df = df[0].str.split('\t', expand=True)
+
+
+D = {}
+for row in df.values:
+    for i, val in enumerate(row[1:]):
+        if val not in D:
+            D[val] = (26-i)
+        else:
+            D[val] += (26-i)
+
+scores = Counter(D).most_common(54)
+
+
+XX = {1:10, 2:9, 3:9, 4:8, 5:8, 6:7, 7:7, 8:7, 9:6, 10:6, 11:6, 12:6, 13:6, 14:5, 15:5, 16:5, 17:5, 18:5, 19:5, 20:5, 21:4, 22:4, 23:4, 24:4, 25:4}
+# Rules per pot as a tuple (a, b) where a: single team max occurence, b: total occurence of single character
+rules = {'a': (2, 3), 'b': (3, 4), 'c': (10, 6), 'd': (10, 120)}
+
+
+def make_pot(a, b, pot):
+    temp = [score[0] for score in scores[a:b]]
+    for val in temp:
+        pots[val] = pot
+        counts[val] = 0
+    return temp
+
+
+def get_next():
+    weights = []
+    for k,v in draft.items():
+        if len(v) != 11:
+            weights.append(v[0])
+    m = min(weights)
+    email = random.choice([k for k, v in draft.items() if v[0] == m and len(v) != 11])
+    return email
+
+
+def assign(email, char, offset):
+    counts[char] += 1
+    draft[email].append(char)
+    draft[email][0] += (12-len(draft[email])) * XX[offset+1] #(25-offset) #
+    return True
+
+
+pots = {}
+info = {}
+draft = {}
+counts = {}
+
+POTA = make_pot(0, 5, 'a')
+POTB = make_pot(5, 13, 'b')
+POTC = make_pot(13, 25, 'c')
+POTD = make_pot(25, 54, 'd')
+
+for email in df[0]:
+    draft[email] = [0]
+    info[email] = {'a':0, 'b':0, 'c':0, 'd':0, 'offset':0}
+
+
+for i in range(5, 0, -1):
+	print(' ', i, ' '*10, end='\r')
+	time.sleep(1)
+
+print(' Shall we begin ? ')
+time.sleep(3)
+
+
+clear = True
+kill = 0
+while kill < 10*len(draft):
+    if clear:
+        os.system('clear')
+
+    email = get_next()
+    assigned = False
+    if len(draft[email]) < 11:
+        index = df.loc[df[0] == email].index.item()
+        while not assigned:
+            offset = info[email]['offset']
+            char = df.loc[index].values[offset+1:][0]
+            count = counts[char]
+            pot = pots[char]
+            rule = rules[pot]
+#             print(email, nex, offset)
+#             print(count, pot, rule)
+            if count < rule[1]:
+                if (pot=='a' and info[email][pot] < rule[0]) or \
+                (pot=='b' and (info[email][pot] + info[email]['a']) < rule[0]) or \
+                pot=='c' or pot=='d':
+                    kill += 1
+                    info[email][pot] += 1
+                    assigned = assign(email, char, offset)
+            
+            info[email]['offset'] += 1
+
+            if clear:
+                print('\n LIVE DRAFT...\n\n')
+                for k, v in draft.items():
+                    print(' ', k, ':', v)
+                    print('', '_'*200)
+                    print()
+            if assigned:
+                clear = True
+                print('  ', kill, ' '+email+'  ---> ', char, '(weight '+str(draft[email][0])+')')
+                # time.sleep(1.2)
+            else:
+                clear = False
+                print('  ', kill+1, ' '+email+'   === OOPS ===   '+char)
+                # time.sleep(1)
+
+os.system('clear')
+print('\n\t\t\t\t\t\t\t\t============================ DRAFT COMPLETE ===============================\n\n')
+for k, v in draft.items():
+    print(' ', k, ':', v)
+    print('', '_'*200)
+    print()           
